f852a51fefc05094d5351c1f67cefd711bf51b5f
Anthony Bengtsson <124379293+1AnthonyB1@users.noreply.github.com>
2023-05-25 14:51:45 +0000

    Improved & Smooth Movement

--- FinalAssigment.py
+++ FinalAssigment.py
@@ -1,35 +1,46 @@
-import pygame, sys
-from pygame.locals import QUIT
-
+import pygame
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((400, 300))
-pygame.display.set_caption('Final Assigment ICS2O')
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-    pygame.display.update()
 
-win = pygame.display.set_mode((500, 500))
+win = pygame.display.set_mode((500,500))
+pygame.display.set_caption("First Game")
 
 x = 50
 y = 50
 width = 40
-hight = 60
-vel = 5
+height = 60
+vel = 3
 
 run = True
+
+Clock = pygame.time.Clock()
+LastTick = 0
 while run:
-  pygame.time.delay(100)
+    Delta = (pygame.time.get_ticks() - LastTick) / 1000
+    LastTick = pygame.time.get_ticks()
+    print(Delta)
 
-for event in pygame.event.get():
-  if event.type == pygame.QUIT:
-    run = False
+  
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
 
-  pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
-  pygame.display.update()
+    keys = pygame.key.get_pressed()
+    
+    if keys[pygame.K_LEFT]:
+        x -= vel
 
+    if keys[pygame.K_RIGHT]:
+        x += vel
+
+    if keys[pygame.K_UP]:
+        y -= vel
+
+    if keys[pygame.K_DOWN]:
+        y += vel
+    
+    win.fill((0,0,0))  # Fills the screen with black
+    pygame.draw.rect(win, (255,0,0), (x, y, width, height))   
+    pygame.display.flip()
+    Clock.tick(60)
+    
 pygame.quit()
-
-
