b08eced1b549adb1cd47e9e9eafe4d54651ab226
Anthony Bengtsson <124379293+1AnthonyB1@users.noreply.github.com>
2023-05-30 14:44:44 +0000

    implement user choices

--- TextBasedGame.py
+++ TextBasedGame.py
@@ -7,11 +7,25 @@ print("Hello",x,"nice to meet you...")
 while True:
     y = input("\nAre you ready to begin now? \nYes or No... ")
     y = str.upper(y)
-    if y == "NO":
-        True
+    if y == "NO" or y == "N":
         print("\nOk then... I guess you'll never witness this epic story...")
-    elif y == "YES":
-        False
+    elif y == "YES" or y == "Y":
         break
 
-z = print("\nGreat! The adventure begins... \n\nYou find yourself on a grassy beach, ahead of you there \nis a sandy hill and to your left the remains of \ncrashed shipwreck.")
+print("\nGreat! The adventure begins... \n\nYou find yourself on a grassy beach, ahead of you there \nis a sandy hill and to your left the remains of crashed \nshipwreck.")
+
+print("\nYou feel an ache in your back as you stand up and you soon \nrelize that you could get a better view of the sourounding\nland if you went up the hill. However you also notice\nsomething glimmering in the hull of the deserted ship. What \ndo you achoose?\n\nOption 1: Climb atop the sandy hill\n\nOption 2: Walk into the hull of the broken ship")
+z = int(input("\nWhat option do you choose (Must be a number):"))
+
+while True:
+    if z == 1:
+        print("\nnice choice")
+        break
+
+    elif z == 2:
+        print("\nInteresting choice")
+        break
+
+    else:
+        print("Flubby Chubb")
+        break
