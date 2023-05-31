dc746b11307b5aa98e73f962d73b208a9387acdf
Anthony Bengtsson <124379293+1AnthonyB1@users.noreply.github.com>
2023-05-31 00:38:52 +0000

    Added in a "Slow Type" Feature

--- TextBasedGame.py
+++ TextBasedGame.py
@@ -1,31 +1,40 @@
+import sys, time, random
+
+typing_speed = 100 #wpm
+def slow_type(t):
+    for l in t:
+        sys.stdout.write(l)
+        sys.stdout.flush()
+        time.sleep(random.random()*10.0/typing_speed)
+
 print()
-x = input("Hello There, I'm ... What's your name? ")
+x = input(slow_type("Hello There, I'm ... What's your name?"))
 print()
 print("Hello",x,"nice to meet you...")
 
 #The adventure begins
 while True:
-    y = input("\nAre you ready to begin now? \nYes or No... ")
+    y = input(slow_type("\nAre you ready to begin now? \nYes or No... "))
     y = str.upper(y)
     if y == "NO" or y == "N":
-        print("\nOk then... I guess you'll never witness this epic story...")
+        slow_type("\nOk then... I guess you'll never witness this epic story...")
     elif y == "YES" or y == "Y":
         break
 
-print("\nGreat! The adventure begins... \n\nYou find yourself on a grassy beach, ahead of you there \nis a sandy hill and to your left the remains of crashed \nshipwreck.")
+slow_type("\nGreat! The adventure begins... \n\nYou find yourself on a grassy beach, ahead of you there \nis a sandy hill and to your left the remains of crashed \nshipwreck.\n")
 
-print("\nYou feel an ache in your back as you stand up and you soon \nrelize that you could get a better view of the sourounding\nland if you went up the hill. However you also notice\nsomething glimmering in the hull of the deserted ship. What \ndo you achoose?\n\nOption 1: Climb atop the sandy hill\n\nOption 2: Walk into the hull of the broken ship")
+slow_type("\nYou feel an ache in your back as you stand up and you soon \nrelize that you could get a better view of the sourounding\nland if you went up the hill. However you also notice\nsomething glimmering in the hull of the deserted ship. What \ndo you achoose?\n\nOption 1: Climb atop the sandy hill\n\nOption 2: Walk into the hull of the broken ship\n")
 z = int(input("\nWhat option do you choose (Must be a number):"))
 
 while True:
     if z == 1:
-        print("\nnice choice")
+        slow_type("\nnice choice")
         break
 
     elif z == 2:
-        print("\nInteresting choice")
+        slow_type("\nInteresting choice")
         break
 
     else:
-        print("\nFlubby Chubb")
+        slow_type("\nFlubby Chubb")
         break
